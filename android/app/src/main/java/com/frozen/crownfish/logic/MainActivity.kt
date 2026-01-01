4c4eed6538fe2be81c4b1e975afd301e83faa371
olya <olya0665643972@gmail.com>
2026-01-01 15:12:03 +0200

    init

--- android/app/src/main/java/com/frozen/crownfish/logic/MainActivity.kt
+++ android/app/src/main/java/com/frozen/crownfish/logic/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.frozencrownfishlogic
+package com.frozen.crownfish.logic
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/frozen/crownfish/logic/MainApplication.kt
+++ android/app/src/main/java/com/frozen/crownfish/logic/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.frozencrownfishlogic
+package com.frozen.crownfish.logic
 
 import android.app.Application
 import com.facebook.react.PackageList
